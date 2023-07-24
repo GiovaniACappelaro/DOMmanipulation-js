5c2432d94c4b274583fe745adb1b180de77c046f
GiovaniACappelaro <giovani2.astolpho@hotmail.com>
2023-07-24 16:08:42 -0300

    types of css selectors and examples

---
+++ 1-favoriteMovie/app.js
@@ -0,0 +1,35 @@
+//DOM manipulation()
+
+
+//GetElementById()
+const title = document.getElementById('main-heading');
+
+console.log(title);
+
+
+//GetElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem);
+
+
+
+//GetElementByTagName()
+const listItem2 = document.getElementsByTagName('li');
+
+console.log(listItem2);
+
+
+//querySelector()
+
+const container = document.querySelector('div');
+
+console.log(container);
+
+
+//querySelectorAll()
+
+const containerAll = document.querySelectorAll('div');
+
+console.log(containerAll);
