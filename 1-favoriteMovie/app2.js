af3b5f0bd0d4def0ac51799b82dde3d9bff7e134
GiovaniACappelaro <giovani2.astolpho@hotmail.com>
2023-07-28 14:25:45 -0300

    Parent node traversal

---
+++ 1-favoriteMovie/app2.js
@@ -0,0 +1,22 @@
+//DOM MANIPULATION
+
+//TRAVERSE THE DOM
+
+//PARENT NODE TRAVERSAL
+let ul = document.querySelector('ul');
+
+/*console.log(ul.parentNode.parentNode);
+console.log(ul.parentElement.parentElement);
+navega do ul "pra cima" - vai até os pais dos pais... 
+
+
+const html = document.documentElement; //elemento raiz do doc (tag html)
+
+console.log(html.parentNode);
+console.log(html.parentElement);
+
+*/
+
+//CHILD NODE TRAVERSAL
+
+
