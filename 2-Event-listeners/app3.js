41cb27290cbb752c033d0699236055a8730833d5
GiovaniACappelaro <giovani2.astolpho@hotmail.com>
2023-08-11 15:37:10 -0300

    mouse over and mouse out listener

--- 2-Event-listeners/app3.js
+++ 2-Event-listeners/app3.js
@@ -4,6 +4,8 @@
 
 //element.addEventListenner('tpe of event', function, <booleano>);
 
+
+//Click
 const buttonTwo = document.querySelector('.btn-2');
 
 function alertBtn(){
@@ -13,3 +15,20 @@ function alertBtn(){
 
 buttonTwo.addEventListener("click", alertBtn);
 
+
+//Mouse over
+const newBackgroundColor = document.querySelector('.box-3');
+
+function changeBGColorOver(){
+    newBackgroundColor.style.background = 'blue';
+}
+
+function changeBGColorOut(){
+    newBackgroundColor.style.background = '#1c1c5f';
+}
+
+newBackgroundColor.addEventListener('mouseover', changeBGColorOver);
+
+newBackgroundColor.addEventListener('mouseout', changeBGColorOut);
+
+//pra esse caso, é muito melhor usar um :hover direto no CSS 
