927f292476e94cd8dc80d5a1619e76635edcae88
GiovaniACappelaro <giovani2.astolpho@hotmail.com>
2023-08-11 15:09:16 -0300

    Click event for event listener

--- 2-Event-listeners/app3.js
+++ 2-Event-listeners/app3.js
@@ -2,3 +2,14 @@
 
 //Event Listeners
 
+//element.addEventListenner('tpe of event', function, <booleano>);
+
+const buttonTwo = document.querySelector('.btn-2');
+
+function alertBtn(){
+    alert('I also love Javascript');
+};
+
+
+buttonTwo.addEventListener("click", alertBtn);
+
