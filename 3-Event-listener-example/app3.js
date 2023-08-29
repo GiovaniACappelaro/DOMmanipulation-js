69ac89769877862bf180aded21980c3ade8bd273
User <user@macbook-pro.dlink>
2023-08-29 11:08:26 -0300

    general stilyng for the Event Listeners example page

---
+++ 3-Event-listener-example/app3.js
@@ -0,0 +1,18 @@
+//DOM Manipulation
+
+//Reveal event
+
+const revealBtn = document.querySelector(".reveal-btn");
+
+const hiddenContent = document.querySelector(".hidden-content");
+
+function revealContent(){
+    if( hiddenContent.classList.contains('revealBtn') ){
+        hiddenContent.classList.remove('reveal-btn');
+    } else{
+        hiddenContent.classList.add('reveal-btn');
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
+
