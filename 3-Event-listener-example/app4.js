c0836e6031be63dae41e862f60629ae5310e3acc
User <user@macbook-pro.dlink>
2023-08-30 14:16:35 -0300

    dom manipulation example - doesnt work properly for bad example

--- 3-Event-listener-example/app4.js
+++ 3-Event-listener-example/app4.js
@@ -2,9 +2,9 @@
 
 //Reveal event
 
-const revealBtn = document.querySelector(".reveal-btn");
+const revealBtn = document.querySelector('.reveal-btn');
 
-const hiddenContent = document.querySelector(".hidden-content");
+const hiddenContent = document.querySelector('.hidden-content');
 
 function revealContent(){
     if( hiddenContent.classList.contains('revealBtn') ){
